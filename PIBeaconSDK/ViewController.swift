bd21fa135613085b89fba7ba4681b5124c02596b
Daniel Choi <udchoi@us.ibm.com>
2015-10-27 13:17:27 -0400

    adding all the files

--- PIBeaconSDK/ViewController.swift
+++ PIBeaconSDK/ViewController.swift
@@ -1,25 +1,155 @@
 //
-//  ViewController.swift
-//  PIBeaconSDK
+//© Copyright 2015 IBM Corp.
 //
-//  Created by Daniel Choi on 8/21/15.
-//  Copyright (c) 2015 Daniel Choi. All rights reserved.
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
 //
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 
 import UIKit
+import PresenceInsightsSDK
+import CoreLocation
 
-class ViewController: UIViewController {
+
+//need to add CLLocationManagerDelegate and PIBeaconSensorDelegate
+//PiBeaconSensorDelegate is needed to run method didRangeBeacons. getting all the ranged beacons 
+//CLLocationManagerDelegate is needed to access and request authoriztion to use ble location
+class ViewController: UIViewController, CLLocationManagerDelegate, PIBeaconSensorDelegate{
+    
+    //initializing the parameters (BM info)
+    //initializing objects: PIBeaconSensor, PIAdapter, CLLocationmanger
+    let locationManager = CLLocationManager()
+    let tenantID = "XXXXXXXXX"
+    let orgID = "XXXXXXXXX"
+    let username = "XXXXXXXXX"
+    let passwd = "XXXXXXXXX"
+    let siteID = "XXXXXXXXX"
+    let floorID = "XXXXXXXXX"
+    
+
+    
+    var piBeaconSensor : PIBeaconSensor!
+    var piAdapter : PIAdapter!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //creating piAdapter with bm credentials/info
+        piAdapter = PIAdapter(tenant: tenantID,
+            org: orgID,
+            username: username,
+            password: passwd)
+        //creating piBeaconsensor with piAdapater that we created above
+        piBeaconSensor = PIBeaconSensor(adapter: piAdapter)
+        
+        //setting piBeaconSensor to self to run didRangeBeacons
+        piBeaconSensor.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //setting locationManager to self
+        locationManager.delegate = self
+ 
+        //checking and asking user for allowing ble location access
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse)
+        {
+            locationManager.requestWhenInUseAuthorization()
+        }
+       
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func startSensing(sender: UIButton) {
+        
+        //sdk call for start sensing
+        //call back is necessary
+        piBeaconSensor.start { (NSError) -> () in
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if(NSError == nil){
+                    self.alert("Success", messageInput: "Started Sensing for Beacons")
+                }
+                else{
+                    self.alert("Error", messageInput: "\(NSError)")
+                }
+            })
+        }
+    }
+    
+    @IBAction func stopSensing(sender: UIButton) {
+        //sdk call to stop beacon sensing
+        piBeaconSensor.stop()
+        alert("Success", messageInput: "Successfully stopped sensing for beacons")
 
+    }
+    
+    @IBAction func getUUID(sender: UIButton) {
+        
+        //getes UUID from PI instance
+        piAdapter.getAllBeaconRegions { (uuids, NSError) -> () in
+            // Do whatever you want with the beacon UUIDs here.
+            
+            //for this app, we will pop up message with the list of UUIDs
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if(NSError == nil){
+                    self.alert("UUIDS", messageInput: "UUID: \(uuids)")
+                }
+                else{
+                    self.alert("Error", messageInput: "\(NSError)")
+                }
+            })
+        }
+    }
+
+    @IBAction func getBeacons(sender: UIButton) {
+        
+        piAdapter.getAllBeacons(siteID, floor: floorID) { (beacons, NSError) -> () in
+            //you can do whatever you want with the beacons that returned.
+            //for demo purposes will display number of beacons
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if(NSError == nil){
+                    let num = beacons.count
+                    self.alert("Number of Beacons", messageInput: "Total number of beacons for the floor: \(num)")
+                }
+                else{
+                    self.alert("Error", messageInput: "\(NSError)")
+                }
+            })
+            
+        }
+        
+       
+    }
+    
+    
+    func didRangeBeacons(beacons: [CLBeacon]) {
+        // Do whatever you want with the ranged beacons here.
+        //anything you want to locally on the app goes here
+        
+        //for testing purposes. will just print out to the console
+        print(beacons)
+    }
+    
+    //function to easily create alert messages
+    func alert(titleInput : String , messageInput : String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+  
+   
 
 }
 
---
+++ README.md
@@ -0,0 +1,48 @@
+#MobileFirst Platform - PI Beacon Sensing 
+The purpose of this app is to show how to implement PI SDK and use for Beacon sensing for IOS (Swift).
+
+##Instructions
+You would need to use a ios device to use Beacon SDK. Xcode simulator does not support beacons. 
+
+* Open PIBeaconSDK.xcodeproj
+* Edit the file ViewController.swift
+	- change the bluemix credentials/information
+		- tenantID, orgID, siteID, floorID, username and password (all this information can be found in your Presence Insights Dashboard)
+* Make sure you select your ios device instead of the simulator
+* Click the play button to build and run
+
+
+##Quick Walkthrough
+ViewController.swift is a great way to see how objects were initialized and implemented.
+
+On the sample app, there are 4 buttons the user can press:
+
+* "Start Sensing"
+	- Device will start scanning for nearby beacons and if the beacon exist in the Presence Insights, it will send that location payload to Presence Insights.
+	- The app will alert the user stating it successfully started scanning.
+* "Get UUID for Beacons"
+	- Grabs all the UUIDs from the Presence Insights and pop up message listing them.
+* "Get Beacons on the Floor"
+	- The app will grab the beacons and alert the user number of beacons on the floor. 
+* "Stop Sensing"
+	- Device will stop scanning for beacons
+	- They app will alert the user stating it has successfully stopped the scanning.
+	
+There is also another function you can implement that gets a list of CLBeacons detected by the locationManager. In this app, it currently just prints out to the console but can implement anything you would like in your app.
+
+
+Copyright 2015 IBM Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
